c70c0f435a4f93746c98af6b4c1f90b6e702436b
CJCrafter <collinjbarber@gmail.com>
2022-05-26 19:14:00 -0700

    shape

--- src/main/java/me/deecaad/weaponmechanicscosmetics/trails/CosmeticsScriptManager.java
+++ src/main/java/me/deecaad/weaponmechanicscosmetics/trails/CosmeticsScriptManager.java
@@ -1,9 +1,9 @@
 package me.deecaad.weaponmechanicscosmetics.trails;
 
 import me.deecaad.weaponmechanics.weapon.projectile.AProjectile;
-import me.deecaad.weaponmechanics.weapon.projectile.ProjectileScript;
 import me.deecaad.weaponmechanics.weapon.projectile.ProjectileScriptManager;
-import me.deecaad.weaponmechanicscosmetics.trails.shape.Spiral;
+import me.deecaad.weaponmechanicscosmetics.trails.shape.FunctionShape;
+import me.deecaad.weaponmechanicscosmetics.trails.shape.Shape;
 import org.bukkit.Color;
 import org.bukkit.Particle;
 import org.bukkit.plugin.Plugin;
@@ -22,11 +22,18 @@ public class CosmeticsScriptManager extends ProjectileScriptManager {
     @Override
     public void attach(@NotNull AProjectile aProjectile) {
         List<ParticleSerializer> list = Arrays.asList(
-                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.RED, Color.WHITE, 1.0f)),
-                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.WHITE, Color.BLUE, 1.0f)),
-                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.BLUE, Color.RED, 1.0f))
+                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.RED, Color.WHITE, 0.25f)),
+                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.WHITE, Color.BLUE, 0.25f)),
+                new ParticleSerializer(Particle.DUST_COLOR_TRANSITION, 1, 1.0f, new Vector(), new Particle.DustTransition(Color.BLUE, Color.RED, 0.25f))
         );
-        Trail trail = new Trail(0.2, Trail.ListChooser.LOOP, list, new Spiral(2.0, 16, 2));
+
+        Shape shape = new FunctionShape(32, 32) {
+            @Override
+            public double radiusFunction(double theta) {
+                return 2 * Math.cos(2 * theta);
+            }
+        };
+        Trail trail = new Trail(0.2, Trail.ListChooser.LOOP, list, shape);
         aProjectile.addProjectileScript(new TrailScript(getPlugin(), aProjectile, trail));
     }
 }
